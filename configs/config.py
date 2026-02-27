20721fa6ad7a29db588c3caf948ecb246be3aafe
Vivek Srivastava <35591033+vicky30mar82@users.noreply.github.com>
2026-02-27 16:05:58 +0530

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -1,3 +1,5 @@
+import torch
+
 DATA_DIR = "data"
 BATCH_SIZE = 16
 NUM_EPOCHS_HEAD = 5
@@ -5,5 +7,5 @@ NUM_EPOCHS_FINE = 20
 LR_HEAD = 1e-3
 LR_FINE = 3e-5
 IMG_SIZE = 224
-DEVICE = "cuda"
+DEVICE = "cuda" "cuda" if torch.cuda.is_available() else "cpu"
 MODEL_NAME = "vit_base_patch16_224"
