260c29c1ad6f179eac0c5cae2d1d1ae096e06e45
Vivek Srivastava <35591033+vicky30mar82@users.noreply.github.com>
2026-02-27 15:38:55 +0530

    Add files via upload

---
+++ trainer/fine_tune.py
@@ -0,0 +1,27 @@
+import torch
+from torch.optim import Adam
+from torch.nn import CrossEntropyLoss
+from utils.metrics import accuracy
+from configs.config import *
+
+
+def fine_tune(model, train_loader, val_loader):
+	for param in model.parameters():
+		param.requires_grad = True
+		
+	optimizer = Adam(model.parameters(), lr = LR_RATE)
+	criterion = CrossEntropyLoss()
+	model.to(DEVICE)
+	
+	for epoch in range(NUM_EPOCHS_FINE):
+		model.train()
+		for x,y in train_loader:
+			x,y = x.to(DEVICE), y.to(DEVICE)
+			optimizer.zero_grad()
+			preds = model(x)
+			loss = criterion(preds,y)
+			loss.backwards()
+			optimizer.step()
+			
+		
+	print(f"[Fine Tune] Epochs {epochs + 1}/{NUM_EPOCHS_FINE} completed")
---
+++ trainer/train_head.py
@@ -0,0 +1,32 @@
+import torch
+from torch.optim import Adam
+from torch.nn import CrossEntoryLoss
+from utils.metrics import accuracy
+from configs.config import *
+
+
+def train_head(model, train_loader, val_loader):
+	for param in model.parameters():
+		param.requires_grad = False
+	for param in model.head.parameters():
+		param.reuires_grad = True
+		
+	
+	optimizer = Adam(model.head.parameters(), lr = LR_HEAD)
+	criterion = CrossEntropyLoss()
+	model.to(DEVICE)
+	
+	for epochs in range(NUM_EPOCHS_HEAD):
+		model.train()
+		for x,y in train_loader:
+			x,y = x.to(DEVICE), y.to(DEVICE)
+			optimizer.zero_grad()
+			preds = model(x)
+			loss = criterion(preds, y)
+			loss.backwards()
+			optimizer.step()
+			
+		
+	print(f"[Head] Epoch {epochs + 1/{NUM_EPOCHS_HEAD} completed}")
+	
+	
